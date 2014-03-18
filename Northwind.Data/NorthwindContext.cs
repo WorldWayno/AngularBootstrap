35c4825c8c400a1fbde7667601f4901bc0acb7a0
wayne <world.wayno@gmail.com>
2014-03-18 07:00:36 -0400

    adding Repository infrastructure

---
+++ Northwind.Data/NorthwindContext.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using Northwind.Data.Mapping;
+
+namespace Northwind.Data
+{
+    public class NorthwindContext : DbContext, IDataContext
+    {
+        static NorthwindContext()
+        {
+            System.Data.Entity.Database.SetInitializer<NorthwindContext>(null);
+        }
+
+        public NorthwindContext() : this("NorthwindConnection")
+        {
+            
+        }
+        public NorthwindContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+              
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new EmployeeMap());
+        }
+
+        public void SyncObjectState(object entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
