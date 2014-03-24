90b89e024477fbdaf36856efbe6043c2ebb8a6bd
wayne <world.wayno@gmail.com>
2014-03-23 20:13:36 -0400

    adding bookController

---
+++ AngularBootstrap/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularBootstrap.Controllers
+{
+    public class BookController : Controller
+    {
+        //
+        // GET: /Book/
+        [HttpGet]
+        public ActionResult Bootstrap()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Angular()
+        {
+            return View();
+        }
+	}
+}
